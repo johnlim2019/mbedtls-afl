ff2034e5b5325033f57d86791f166ad29a9648ee
johnlim2019 <69998939+johnlim2019@users.noreply.github.com>
2023-03-18 13:21:45 +0800

    python runner update, left seedQ methods to retrieve seed and to populate queue with prepared seeds (see the aes_combined_seed folder)

--- project_testing/python/lib.py
+++ project_testing/python/lib.py
@@ -5,8 +5,9 @@ class Runner:
     hashList = []
     SeedQDict = {}
     pathQDict = {}
-    seedFile:str = None
+    seedFile:str = './python/seed.txt'
     projectTestingDir = None
+    coverageFile:str = None
     
     def __init__(self,pwd:str):
         self.projectTestingDir = pwd
@@ -21,23 +22,23 @@ class Runner:
         fileStr += key2 + "\n"
         fileStr += IV + "\n"
         fileStr += IV2
-        with open('./python/seed.txt', 'w') as f:
+        with open(self.seedFile, 'w') as f:
             f.write(fileStr)
-        return './python/seed.txt'
+        return self.seedFile
 
 
     def isCrash(self,path: dict) -> bool:
         return path[375] == 0
 
 
-    def crashNoPathHandler(self)->dict:
+    def crashNoPathCov(self)->dict:
         # we create a path where 375 is 0 this is the main method return success line
         return {375:0}
 
-    def runScriptUbuntu(self,seedFilePath: str) -> int:
-        # 0 no crash path generated
-        # -1 crash no path generated
-        # 1 crash with path generated
+    def runScriptUbuntu(self) -> int:
+        # 0 no crash, path generated
+        # -1 crash, no path generated
+        # 1 crash, with path generated
         # 2 some execution failure not related to test script
         # we need to compile and then execute binary to be able to run coverage.
         print()
@@ -51,7 +52,7 @@ class Runner:
         if os.system(compileStr) != 0:
             print("did not compile")
             return 1
-        runTest = "./crypt_test " + seedFilePath
+        runTest = "./crypt_test " + self.seedFile
         exitCode = os.waitstatus_to_exitcode(os.system(runTest))
         if exitCode > 1: 
             print("Program crash no path generated")
@@ -65,7 +66,7 @@ class Runner:
         print("completed one cycle")
         return exitCode
 
-    def getPathCovFile(self,input:dict)->str:
+    def getPathCovFile(self,input:dict)->int:
         # run the test and return 
         key = input["key"]
         key2 = input["key2"]
@@ -76,38 +77,62 @@ class Runner:
         seedFilePath = self.createSeedFile(key,key2,iv,iv2,algo,plain)
         print("seed file path")
         print(seedFilePath)
-        exitStatus = self.runScriptUbuntu(seedFilePath)
-        if exitStatus == 0 or exitStatus == 1:
-            return self.projectTestingDir+"/crypt_test.c.gcov"
-        elif exitStatus == -1:
-            return "No gcov file"
-        elif exitStatus == 2:
-            return None
-        else:
-            return None
+        self.coverageFile = seedFilePath
+        exitStatus = self.runScriptUbuntu()
+        return exitStatus
 
     def parseFile(self,covFilePath:str)->dict:
         return {}
     
-    def isInteresting(self,path:dict)->bool:
+    def isInteresting(self,path:dict,path2:dict)->bool:
         return True
     
+    def isInterestingOuter(self,newpath:dict)->bool:
+        for oldpathId in self.hashList:
+            oldpath = self.pathQDict[oldpathId]
+            if self.isInteresting(oldpath,newpath) != False:
+                return False
+        return True
+
+    
     def runTest(self,inputDict)->None:
-        coverageFile:str = self.getPathCovFile(inputDict)
+        exitCode:int = self.getPathCovFile(inputDict)
+        isfail = False
+        if exitCode == 2:
+            print("gcc compiling issue or gcov execution issue")
+            return exit()
+        elif exitCode == -1:
+            isfail = True
+            path:dict = self.crashNoPathCov()
+        elif exitCode == 1:
+            isfail = True
+        elif exitCode == 0:
+            isfail = False
+        else:
+            print("unknown exitcode crash")
+            exit()
+        covFilePath = self.coverageFile
         path:dict = self.parseFile(covFilePath)
-        interesting:bool = self.isInteresting(path)
+        interesting:bool = self.isInterestingOuter(path)
         if interesting:
             ids = uuid.uuid4()
-            self.SeedQDict[ids] = input_dict
+            self.SeedQDict[ids] = fuzzed_seed
             self.SeedQDict[ids] = path
-            self.seedFile
+            self.pathQDict[ids] = path
+            self.seedFile = exitCode
 
-
+    def getInput(self):
+        return
+        ids = self.hashList[-1]
+        return self.SeedQDict[ids]
+    
 if __name__ == "__main__":
-    pwd = "/home/limjieshengubuntu/mbedtls-afl/project_testing"
+    pwd = "/home/lim/mbedtls/project_testing"
 
-    # fuzzing to get input dict 
-    input_dict = {
+    runner = Runner(pwd)
+    seed = runner.getInput() 
+    # fuzzing to get fuzzed input dict with fuzzer class
+    fuzzed_seed = {
         "key":"itzkbg2",
         "key2":"itzkbg2",
         "iv":"0123456789123456",
@@ -116,13 +141,8 @@ if __name__ == "__main__":
         "plain":"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+-=~[];',./{}|:?<>\"123"
     }
 
-    runner = Runner(pwd)
-    # print(runner.projectTestingDir)
-    covFilePath = runner.getPathCovFile(input_dict)
-    print(covFilePath)
-    # print(runner.runScriptUbuntu("aes_combined_seed/aes_combined_cbc_keynomatch.txt"));
-    print(runner.runScriptUbuntu("aes_combined_seed/aes_combined_cbc.txt"));
-    
+    # run coverage and return coverage file
+    covFilePath = runner.getPathCovFile(fuzzed_seed)    
 
 
 
